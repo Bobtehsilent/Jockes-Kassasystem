9b3d3aeed5a0de0ad5a9cefe7d3d000d8a92309f
Joakim Norborg <jocke4300@gmail.com>
2023-10-17 15:39:37 +0200

    New version.

--- Kampanj.py
+++ Kampanj.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 class Kampanj:
-    def __init__(self, kampanj_namn, produkt_id, kampanj_pris, kampanj_start_datum, kampanj_slut_datum):
+    def __init__(self, kampanj_namn, produkt_id, kampanj_pris, 
+                 kampanj_start_datum, kampanj_slut_datum):
         self.__kampanj_namn = kampanj_namn
         self.__produkt_id = produkt_id
         self.__kampanj_pris = kampanj_pris
@@ -41,7 +42,7 @@ class Kampanj:
 
     @kampanj_start_datum.setter
     def kampanj_start_datum(self, nytt_start_datum):
-        self.__kampanj_start_datum = nytt_start_datum
+        self.__kampanj_start_datum = self.analysera_och_validera(nytt_start_datum)
 
     @property
     def kampanj_slut_datum(self):
@@ -49,17 +50,15 @@ class Kampanj:
 
     @kampanj_slut_datum.setter
     def kampanj_slut_datum(self, nytt_slut_datum):
-        self.__kampanj_slut_datum = nytt_slut_datum
+        self.__kampanj_slut_datum = self.analysera_och_validera(nytt_slut_datum)
     
     @staticmethod
     def analysera_och_validera(datumstr):
         if isinstance(datumstr, datetime):
-            #print("Received a datetime object, returning as-is.")
             return datumstr
         elif isinstance(datumstr, str):
             format = "%Y-%m-%d"
             try:
-                #print("Attempting to parse string to datetime.")
                 return datetime.strptime(datumstr, format)
             except ValueError:
                 print("Gick inte att analysera strängen. ogiltigt format.")
@@ -67,24 +66,6 @@ class Kampanj:
         else:
             print("Received an unexpected type, raising ValueError.")
             raise ValueError("Oväntad typ: förväntar str eller datetime")
-
-
-    def är_aktiv(self):
-        idag = datetime.today()
-        return self.kampanj_start_datum <= idag <= self.kampanj_slut_datum
-    
-    def gällande_pris(self, original_pris):
-        if self.är_aktiv():
-            return self.kampanj_pris
-        else:
-            return original_pris
-
-    def kampanjdagar_kvar(self):
-        if self.är_aktiv():
-            dagar_kvar = (self.kampanj_slut_datum - datetime.today()).days
-            return dagar_kvar
-        else:
-            return 0
 
     def till_dict(self):
         return {
@@ -96,4 +77,5 @@ class Kampanj:
         }
 
     def __str__(self):
-        return f"Startdatum: {self.kampanj_start_datum} Slutdatum: {self.kampanj_slut_datum} kampanjpris: {self.kampanj_pris}"
+        return (f"Startdatum: {self.kampanj_start_datum} Slutdatum: "
+            f"{self.kampanj_slut_datum} kampanjpris: {self.kampanj_pris}")
